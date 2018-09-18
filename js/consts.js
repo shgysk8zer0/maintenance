90494fa0d02a2d377ea39389b056d9c1737456b7
Chris Zuber <shgysk8zer0@gmail.com>
2018-09-18 10:11:18 -0700

    Move maintenance table to template file

--- js/consts.js
+++ js/consts.js
@@ -1,6 +1,7 @@
 export const API = 'https://www.vrmtel.net/api/v1/slapi.php';
 export const TEMPLATES = [
 	new URL('templates/login-form.html', document.baseURI),
+	new URL('templates/maintenance-table.html', document.baseURI),
 	new URL('templates/maintenance-item.html', document.baseURI),
 ];
 export const ELEMENTS = [
