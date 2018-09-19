5e43c6f3583c45df451f82fe413cd895874ef944
Chris Zuber <shgysk8zer0@gmail.com>
2018-09-19 12:15:17 -0700

    Improve structure of response objects

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import './std-js/shims.js';
 import {ready, notify, $} from './std-js/functions.js';
 import LoginForm from './components/login-form.js';
 import MaintenanceTable from './components/maintenance-table.js';
-import {API, TEMPLATES, ELEMENTS} from './consts.js';
+import {API, TEMPLATES, ELEMENTS, IMAGES_DIR} from './consts.js';
 
 async function loadTemplate(url) {
 	const resp = await fetch(url);
@@ -59,10 +59,46 @@ async function init() {
 				const json = await resp.json();
 				if (json.hasOwnProperty('error') && json.hasOwnProperty('message')) {
 					throw new Error(`${json.message} [${json.error}]`);
+				} else if (Array.isArray(json)) {
+					const vehicles = json.reduce((vehicles, item) => {
+						let vehicle = vehicles.find(vehicle => vehicle.name === item.vehicle);
+						if (typeof vehicle === 'undefined') {
+							vehicle = {
+								name: item.vehicle,
+								image: item.vehicle_image === '' ? null : new URL(item.vehicle_image, IMAGES_DIR),
+								mileage: parseInt(item.current_mileage),
+								uid: parseInt(item.vehicles_uid),
+								maintenance: []
+							};
+							vehicles.push(vehicle);
+						}
+						vehicle.maintenance.push({
+							uid: parseInt(item.services_uid),
+							description: item.description,
+							scheduled: {
+								miles: parseInt(item.mileage_when_serviced) + parseInt(item.frequencymiles),
+								date: new Date(item.service_dttm.replace(' ', 'T')),
+								locationName: item.name,
+							},
+							lastService: {
+								miles: parseInt(item.mileage_when_serviced),
+								date: new Date(item.service_dttm.replace(' ', 'T')),
+							},
+							reminder: {
+								miles: parseInt(item.remind_x_miles),
+								date: new Date(item.reminder_dttm.replace(' ', 'T')),
+							},
+							frequencyMiles: parseInt(item.frequencymiles),
+							status: item.log_status,
+							priority: parseInt(item.priority),
+						});
+						return vehicles;
+					}, []);
+					console.log(vehicles);
+					console.info(json);
+					maintenance.addItem(...json);
+					maintenance.hidden = false;
 				}
-				console.log(json);
-				maintenance.addItem(...json);
-				maintenance.hidden = false;
 			} catch(err) {
 				console.error(err);
 			}
