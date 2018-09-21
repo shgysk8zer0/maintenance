f259fbb41d06df0ae906d62fe0ae61ce45409c48
Chris Zuber <shgysk8zer0@gmail.com>
2018-09-21 10:59:20 -0700

    Add PWA resources
    Adds manifest, more icons, & service worker

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import './std-js/deprefixer.js';
 import './std-js/shims.js';
-import {ready} from './std-js/functions.js';
+import {ready, registerServiceWorker} from './std-js/functions.js';
 import {init, whenDefined} from './functions.js';
 import './components/login-button.js';
 import './components/logout-button.js';
@@ -10,6 +10,7 @@ import './components/login-form.js';
 import './components/current-year.js';
 import './components/copy-button.js';
 window.whenDefined = whenDefined;
+registerServiceWorker('service-worker.js');
 
 ready().then(async () => {
 	document.documentElement.classList.replace('no-js', 'js');
