1055f7757880ec3ffd02ba696aefad9c2f9c5529
Chris Zuber <shgysk8zer0@gmail.com>
2018-09-24 16:45:54 -0700

    Enhance SVGIcons custom element

--- js/components/svg-icons.js
+++ js/components/svg-icons.js
@@ -1,10 +1,35 @@
 export default class SVGIcons extends HTMLElement {
 	constructor() {
 		super();
-		this.imported = false;
 		this.hidden = true;
+		this.addEventListener('stateChange', event => {
+			switch(event.detail.newValue) {
+			case 'complete':
+				this.dispatchEvent(new Event('load'));
+				break;
+			case 'importing':
+				this.dispatchEvent(new CustomEvent('loadStart'));
+				break;
+			}
+		});
+
+		this.addEventListener('sourceChange', () => {
+			this.state = 'pending';
+			[...this.children].forEach(child => child.remove());
+			this.import();
+		});
+
+		this.addEventListener('error', err => {
+			console.error(err.error);
+			this.state = 'failed';
+		});
+
+		this.addEventListener('load', () => {
+			this.state = 'complete';
+		});
+
 		if (this.hasAttribute('src')) {
-			this.import().catch(console.error);
+			this.import();
 		}
 	}
 
@@ -15,17 +40,87 @@ export default class SVGIcons extends HTMLElement {
 	}
 
 	set src(src) {
-		this.imported = false;
-		this.setAttribute('src', src);
-		this.import();
+		const detail = {
+			newValue: src,
+			oldValue: this.src,
+		};
+		if (src !== this.src) {
+			this.setAttribute('src', src);
+			this.dispatchEvent(new CustomEvent('sourceChange', {detail}));
+		}
+	}
+
+	get state() {
+		return this.getAttribute('state') || 'pending';
+	}
+
+	get pending() {
+		return this.state === 'pending';
+	}
+
+	get failed() {
+		return this.state === 'failed';
+	}
+
+	set state(newValue) {
+		const oldValue = this.state;
+		if (newValue !== oldValue) {
+			const detail = {oldValue, newValue};
+			this.setAttribute('state', newValue);
+			this.dispatchEvent(new CustomEvent('stateChange', {detail}));
+		}
 	}
 
 	get imported() {
-		return this.hasAttribute('imported');
+		return this.state === 'complete';
 	}
 
-	set imported(val) {
-		this.toggleAttribute('imported', !!val);
+	get mode() {
+		return this.getAttribute('mode') || this.src.host === location.host ? 'same-origin' : 'cors';
+	}
+
+	set mode(mode) {
+		this.setAttribute('mode', mode);
+	}
+
+	get integrity() {
+		return this.getAttribute('integrity');
+	}
+
+	set integrity(hash) {
+		this.setAttribute('integrity', hash);
+	}
+
+	get credentials() {
+		return this.getAttribute('credentials') || 'omit';
+	}
+
+	set credentials(credentials) {
+		this.setAttribute('credentials', credentials);
+	}
+
+	get cache() {
+		return this.getAttribute('cache') || 'default';
+	}
+
+	set cache(cache) {
+		this.setAttribute('cache', cache);
+	}
+
+	get referrer() {
+		return this.getAttribute('referrer') || 'client';
+	}
+
+	set referrer(referrer) {
+		this.setAttribute('referrer', referrer);
+	}
+
+	get redirect() {
+		return this.hasAttribute('redirect');
+	}
+
+	set redirect(val) {
+		this.toggleAttribute('redirect', val);
 	}
 
 	get symbols() {
@@ -94,11 +189,18 @@ export default class SVGIcons extends HTMLElement {
 	}
 
 	async ready() {
-		return await new Promise(resolve => {
+		return await new Promise((resolve, reject) => {
 			if (this.imported) {
 				resolve(this);
-			} else {
+			} else if (! this.failed) {
 				this.addEventListener('load', () => resolve(this), {once: true});
+				this.addEventListener('error', err => reject(err.error), {once: true});
+
+				if (this.pending) {
+					this.import();
+				}
+			} else {
+				reject(new Error('Rejecting due to previous error'));
 			}
 		});
 	}
@@ -106,15 +208,45 @@ export default class SVGIcons extends HTMLElement {
 	async import() {
 		if (this.imported) {
 			throw new Error('Already imported');
+		} else if (this.failed) {
+			throw new Error(`Attempting to import from a "${this.state}" state`);
 		} else if (this.hasAttribute('src')) {
-			const resp = await fetch(this.src);
-			const svg = await resp.text();
-			const parser = new DOMParser();
-			const doc = parser.parseFromString(svg, 'image/svg+xml');
-			[...this.children].forEach(child => child.remove());
-			this.append(doc.rootElement);
-			this.dispatchEvent(new Event('load'));
-			this.imported = true;
+			try {
+				this.state = 'importing';
+				const config = {
+					mode: this.mode,
+					cache: this.cache,
+					referrer: this.referrer,
+					credentials: this.credentials,
+					redirect: this.redirect ? 'follow' : 'error',
+				};
+				if (this.hasAttribute('integrity')) {
+					config.integrity = this.integrity;
+				}
+
+				const resp = await fetch(this.src, config);
+				if (resp.ok) {
+					if (! resp.headers.get('Content-Type').startsWith('image/svg+xml')) {
+						throw new Error(`Invalid Content-Type: ${resp.headers.get('Content-Type')}`);
+					}
+					const svg = await resp.text();
+					const parser = new DOMParser();
+					const doc = parser.parseFromString(svg, 'image/svg+xml');
+					[...this.children].forEach(child => child.remove());
+					this.append(doc.rootElement);
+					this.state = 'complete';
+				} else {
+					throw new Error(`${resp.url} [${resp.status} ${resp.statusText}]`);
+				}
+			} catch (err) {
+				this.dispatchEvent(new ErrorEvent('error', {
+					error: err,
+					message: err.message,
+					lineno: err.lineNumber,
+					colno: err.columnNumber,
+					filename: err.fileName,
+				}));
+			}
 		} else {
 			throw new Error('No src attribute set to import from');
 		}
