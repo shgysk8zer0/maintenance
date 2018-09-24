6812a2336c68d80218740beff8593ca947fdebc1
Chris Zuber <shgysk8zer0@gmail.com>
2018-09-24 13:57:17 -0700

    Update copy-button

--- js/components/copy-button.js
+++ js/components/copy-button.js
@@ -2,12 +2,28 @@ export default class CopyButton extends HTMLButtonElement {
 	constructor() {
 		super();
 		if (typeof navigator.clipboard === 'object' && navigator.clipboard.writeText instanceof Function) {
-			this.addEventListener('click', () => navigator.clipboard.writeText(this.dataset.copy));
+			this.addEventListener('click', () => {
+				const target = this.target;
+				if (target instanceof Element) {
+					navigator.clipboard.writeText(('value' in target) ? target.value : target.textContent);
+				} else {
+					throw new Error('No target to copy from');
+				}
+			});
 			this.hidden = false;
 		} else {
 			this.hidden = true;
 		}
 	}
+
+	get target() {
+		return this.ownerDocument.querySelector(this.getAttribute('target'));
+	}
+
+	set target(selector) {
+		this.setAttribute('target', selector);
+	}
 }
+
 
 customElements.define('copy-button', CopyButton, {extends: 'button'});
